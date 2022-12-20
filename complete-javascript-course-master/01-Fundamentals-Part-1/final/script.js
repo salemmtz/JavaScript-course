47dc8e4ab28a16d586d05578c3bc7fb754ff6609
Salem <angeldesalem@gmail.com>
2022-12-20 14:41:14 -0600

    Fundamentals 1 mod

--- complete-javascript-course-master/01-Fundamentals-Part-1/final/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/final/script.js
@@ -207,7 +207,6 @@ HINT: Use an if/else statement 😉
 
 GOOD LUCK 😀
 */
-
 /*
 const massMark = 78;
 const heightMark = 1.69;
@@ -224,30 +223,30 @@ const BMIJohn = massJohn / (heightJohn * heightJohn);
 console.log(BMIMark, BMIJohn);
 
 if (BMIMark > BMIJohn) {
-  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
 } else {
-  console.log(`John's BMI (${BMIJohn}) is higher than Marks's (${BMIMark})!`)
+  console.log(`John's BMI (${BMIJohn}) is higher than Marks's (${BMIMark})!`);
 }
 
 ////////////////////////////////////
 // Type Conversion and Coercion
 
 // type conversion ---- manually converted
-const inputYear = '1991';
+const inputYear = "1991";
 console.log(Number(inputYear), inputYear);
 console.log(Number(inputYear) + 18);
 
-console.log(Number('Jonas'));
+console.log(Number("Jonas"));
 console.log(typeof NaN);
 
 console.log(String(23), 23);
 
 // type coercion ---- automatically converted by JS
-console.log('I am ' + 23 + ' years old');
-console.log('23' - '10' - 3);
-console.log('23' / '2');
+console.log("I am " + 23 + " years old"); // + operators converts numbers into strings
+console.log("23" - "10" - 3); // - operator triggers opposite
+console.log("23" / "2");
 
-let n = '1' + 1; // '11'
+let n = "1" + 1; // '11'
 n = n - 1;
 console.log(n);
 
@@ -257,46 +256,52 @@ console.log(n);
 // 5 falsy values: 0, '', undefined, null, NaN
 console.log(Boolean(0));
 console.log(Boolean(undefined));
-console.log(Boolean('Jonas'));
+console.log(Boolean("Jonas"));
 console.log(Boolean({}));
-console.log(Boolean(''));
+console.log(Boolean(""));
 
 const money = 100;
 if (money) {
   console.log("Don't spend it all ;)");
 } else {
-  console.log('You should get a job!');
+  console.log("You should get a job!");
 }
 
 let height = 0;
 if (height) {
-  console.log('YAY! Height is defined');
+  console.log("YAY! Height is defined");
 } else {
-  console.log('Height is UNDEFINED');
+  console.log("Height is UNDEFINED");
 }
 
 ////////////////////////////////////
 // Equality Operators: == vs. ===
-const age = '18';
-if (age === 18) console.log('You just became an adult :D (strict)');
+// == equality operator does coersion
+// '18' == 18 --> true
+// === equality operator does not do coersin
+// '18' === 18 --> false
 
-if (age == 18) console.log('You just became an adult :D (loose)');
+const age = "18";
+if (age === 18) console.log("You just became an adult :D (strict)");
+
+if (age == 18) console.log("You just became an adult :D (loose)");
 
 const favourite = Number(prompt("What's your favourite number?"));
 console.log(favourite);
 console.log(typeof favourite);
 
-if (favourite === 23) { // 22 === 23 -> FALSE
-  console.log('Cool! 23 is an amzaing number!')
+if (favourite === 23) {
+  // 22 === 23 -> FALSE
+  console.log("Cool! 23 is an amzaing number!");
 } else if (favourite === 7) {
-  console.log('7 is also a cool number')
+  console.log("7 is also a cool number");
 } else if (favourite === 9) {
-  console.log('9 is also a cool number')
+  console.log("9 is also a cool number");
 } else {
-  console.log('Number is not 23 or 7 or 9')
+  console.log("Number is not 23 or 7 or 9");
 }
 
-if (favourite !== 23) console.log('Why not 23?');
+if (favourite !== 23) console.log("Why not 23?");
 
 ////////////////////////////////////
 // Logical Operators
@@ -317,9 +322,9 @@ const isTired = false; // C
 console.log(hasDriversLicense && hasGoodVision && isTired);
 
 if (hasDriversLicense && hasGoodVision && !isTired) {
-  console.log('Sarah is able to drive!');
+  console.log("Sarah is able to drive!");
 } else {
-  console.log('Someone else should drive...');
+  console.log("Someone else should drive...");
 }
 */
 
