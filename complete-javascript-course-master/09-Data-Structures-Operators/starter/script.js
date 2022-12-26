ed8ac7e35d72596a8e3ed2b5111375d0e6820cdd
salemmtz <angeldesalem@gmail.com>
2022-12-26 10:33:56 -0600

    Change

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -119,3 +119,9 @@ const restaurant = {
 // });
 
 /////////////// The spread operator ///////////////
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
