7dd2094aa677d8902afa0603f65a6fe679eaec92
Salem <angeldesalem@gmail.com>
2023-01-18 00:50:14 -0600

    OOP almost done

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -481,32 +481,51 @@
 
 ////////////////////////////// Another class example //////////////////////////////
 
+/// 1) Public fields
+/// 2) Private fields
+/// 3) Public methods
+/// 4) Private methods
+
 class Account {
+  /// 1) Public fields (indstances)
+  locale = navigator.language;
+
+  /// 2) Private fields
+  #movements = [];
+  #pin;
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
-    this.locale = navigator.language;
+
+    /// Protected property
+    this.#pin = pin;
+    // this._movements = [];
+
+    // this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
   /// Public interface of the objcet
+  getMovements() {
+    return this.#movements;
+  }
+
   deposit(val) {
-    this.movements.push(val);
+    this.#movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
   }
 
-  approveLoan(val) {
+  _approveLoan(val) {
     return true;
   }
 
   requestLoan(val) {
-    if (this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`);
     }
@@ -518,5 +537,6 @@ const acc1 = new Account('Jonas', 'EUR', 1111);
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
+console.log(acc1.getMovements());
 
 console.log(acc1);
